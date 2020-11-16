c3c4a65e4bfce428ca0edae28049e0b71f8f40fb
Neil Mehta <neil.mehta310501@gmail.com>
2020-11-16 20:14:19 +0530

    made the noteCard into random colours

--- app/src/main/java/com/bitspilani/inventorytrackerjava/model/Adapter.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/model/Adapter.java
@@ -7,11 +7,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bitspilani.inventorytrackerjava.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     List<String> titles;
@@ -33,6 +36,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
         holder.noteTitle.setText(titles.get(position));
         holder.noteContent.setText(content.get(position));
+        holder.mCardView.setCardBackgroundColor(holder.view.getResources().getColor(getRandomColour()));
 
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +46,24 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         });
     }
 
+    private int getRandomColour() {
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
+    }
+
     @Override
     public int getItemCount() {
         return titles.size();
@@ -49,11 +71,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView noteTitle,noteContent;
+        CardView mCardView;
         View view;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             noteContent = itemView.findViewById(R.id.content);
             noteTitle = itemView.findViewById(R.id.titles);
+            mCardView = itemView.findViewById(R.id.noteCard);
             view = itemView;
         }
     }
