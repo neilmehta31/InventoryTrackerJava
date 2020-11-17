77fef86e86f987f45d91985331bf3130f9e2bfd5
Neil Mehta <neil.mehta310501@gmail.com>
2020-11-17 15:23:45 +0530

    showing firebase data into the app!!!!!!!!!!

--- app/src/main/java/com/bitspilani/inventorytrackerjava/dashboard.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/dashboard.java
@@ -2,10 +2,13 @@ package com.bitspilani.inventorytrackerjava;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,17 +17,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.bitspilani.inventorytrackerjava.model.Adapter;
+import com.bitspilani.inventorytrackerjava.model.Note;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
@@ -32,6 +42,8 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
     NavigationView nav_view;
     RecyclerView noteLists;
     Adapter adapter;
+    FirebaseFirestore fStore;
+    FirestoreRecyclerAdapter<Note,NoteViewHolder> noteAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,6 +51,44 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
         setContentView(R.layout.activity_main_dashboard);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fStore = FirebaseFirestore.getInstance();
+
+        Query query = fStore.collection("notes")
+                .orderBy("title",Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Note> allNotes = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class)
+                .build();
+
+        noteAdapter = new FirestoreRecyclerAdapter<Note, NoteViewHolder>(allNotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int i, @NonNull Note note) {
+                noteViewHolder.noteTitle.setText(note.getTitle());
+                noteViewHolder.noteContent.setText(note.getContent());
+                final int colorCode = getRandomColour();
+                noteViewHolder.mCardView.setCardBackgroundColor(noteViewHolder.view.getResources().getColor(colorCode,null));
+
+                noteViewHolder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(view.getContext(), NoteDetails.class);
+                        intent.putExtra("title",note.getTitle());
+                        intent.putExtra("content", note.getContent());
+                        intent.putExtra("colorCode",colorCode);
+                        view.getContext().startActivity(intent);
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view_layout,parent,false);
+                return new NoteViewHolder(view);
+            }
+        };
 
         noteLists = findViewById(R.id.notelist);
 
@@ -50,21 +100,9 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
 //        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        List<String> titles = new ArrayList<>();
-        List<String> content = new ArrayList<>();
 
-        titles.add("first note title");
-        content.add("first note Content sample");
-
-        titles.add("second note title");
-        content.add("second note Content sample.second note Content sample.second note Content sample.second note Content sample.second note Content sample.second note Content sample.second note Content sample.second note Content sample.second note Content sample.");
-
-        titles.add("third note title");
-        content.add("third note Content sample");
-
-        adapter = new Adapter(titles,content);
         noteLists.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
-        noteLists.setAdapter(adapter);
+        noteLists.setAdapter(noteAdapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -100,5 +138,50 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
             Toast.makeText(this, "Settings Menu is clicked", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+        TextView noteTitle,noteContent;
+        CardView mCardView;
+        View view;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteContent = itemView.findViewById(R.id.content);
+            noteTitle = itemView.findViewById(R.id.titles);
+            mCardView = itemView.findViewById(R.id.noteCard);
+            view = itemView;
+        }
+    }
+
+    private int getRandomColour() {
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(noteAdapter!=null){
+            noteAdapter.stopListening();
+        }
     }
 }
