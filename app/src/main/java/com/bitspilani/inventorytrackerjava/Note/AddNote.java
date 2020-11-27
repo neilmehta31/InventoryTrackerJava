f9fd57ebfd85d64d8c5e604cf95fa4ce10427bb8
AtishayJain11 <atishayjain11122000@gmail.com>
2020-11-28 01:05:05 +0530

    Sabse badi cheez complete #Achievement Notification

--- app/src/main/java/com/bitspilani/inventorytrackerjava/Note/AddNote.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/Note/AddNote.java
@@ -1,18 +1,15 @@
 package com.bitspilani.inventorytrackerjava.Note;
 
 import android.app.AlarmManager;
-import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
-import com.bitspilani.inventorytrackerjava.AlarmBroadcast;
-import com.bitspilani.inventorytrackerjava.Database.DatabaseClass;
-import com.bitspilani.inventorytrackerjava.Database.EntityClass;
 import com.bitspilani.inventorytrackerjava.R;
+import com.bitspilani.inventorytrackerjava.ReminderBroadcast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,7 +19,6 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -31,21 +27,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddNote extends AppCompatActivity implements View.OnClickListener{
+public class AddNote extends AppCompatActivity {
     FirebaseFirestore fstore;
     EditText noteContent, noteTitle;
     ProgressBar progressBarSave;
@@ -53,7 +42,7 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener{
     Intent data;
     Button button_date, button_time;
     String timeTonotify;
-    DatabaseClass databaseClass;
+
 
 
     @Override
@@ -62,15 +51,21 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_add_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createNotificationChannel();
         button_date = findViewById(R.id.button_date);
         button_time = findViewById(R.id.button_time);
-        button_time.setOnClickListener(this);
-        button_date.setOnClickListener(this);
-        databaseClass = DatabaseClass.getDatabase(getApplicationContext());
-
-
-
-
+        button_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddNote.this, "Reminder", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AddNote.this, ReminderBroadcast.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(AddNote.this,0,intent,0);
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                long timeAtButtonClick = System.currentTimeMillis();
+                long tenSecondsInMillis = 1000*10;
+                alarmManager.set(AlarmManager.RTC_WAKEUP,timeAtButtonClick+tenSecondsInMillis,pendingIntent);
+            }
+        });
 
 
         data = getIntent();
@@ -90,7 +85,7 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener{
                 String nTitle = noteTitle.getText().toString();
                 String ncontent = noteContent.getText().toString();
 
-                if(nTitle.isEmpty() || ncontent.isEmpty()){
+                if (nTitle.isEmpty() || ncontent.isEmpty()) {
                     Toast.makeText(AddNote.this, "Cannot save notes with Empty Fields", Toast.LENGTH_SHORT).show();
                     return;
 
@@ -101,9 +96,9 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener{
                 //save note
                 DocumentReference docref = fstore.collection("notes").document(user.getUid())
                         .collection("myNotes").document();
-                Map<String,Object> note = new HashMap<>();
-                note.put("title",nTitle);
-                note.put("content",ncontent);
+                Map<String, Object> note = new HashMap<>();
+                note.put("title", nTitle);
+                note.put("content", ncontent);
 
                 docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -127,129 +122,28 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.close_menu,menu);
+        inflater.inflate(R.menu.close_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.close){
+        if (item.getItemId() == R.id.close) {
             Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onClick(View view) {
-        if(view == button_time){
-            setTime();
-        }
-        else if (view == button_date){
-            setDate();
-        }
-        else{
-            submit();
-        }
-    }
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void submit() {
-
-
-                EntityClass entityClass = new EntityClass();
-                String value = (noteTitle.getText().toString().trim());
-                String date = (button_date.getText().toString().trim());
-                String time = (button_time.getText().toString().trim());
-                entityClass.setEventdate(date);
-                entityClass.setEventname(value);
-                entityClass.setEventtime(time);
-                databaseClass.EventDao().insertAll(entityClass);
-                setAlarm(value, date, time);
-
-
-    }
-
-
-    private void setTime() {
-        Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int i, int i1) {
-                 timeTonotify = i + ":" + i1;
-                button_time.setText(FormatTime(i, i1));
-            }
-        }, hour, minute, false);
-        timePickerDialog.show();
-
-    }
-
-    private void setDate() {
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-
-
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                button_date.setText(day + "-" + (month + 1) + "-" + year);
-            }
-        }, year, month, day);
-        datePickerDialog.show();
-    }
-
-    public String FormatTime(int hour, int minute) {
-
-        String time;
-        time = "";
-        String formattedMinute;
-
-        if (minute / 10 == 0) {
-            formattedMinute = "0" + minute;
-        } else {
-            formattedMinute = "" + minute;
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "LemubitReminderChannel";
+            String description = "Channel for Lemubit Reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Lemubit", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
 
-
-        if (hour == 0) {
-            time = "12" + ":" + formattedMinute + " AM";
-        } else if (hour < 12) {
-            time = hour + ":" + formattedMinute + " AM";
-        } else if (hour == 12) {
-            time = "12" + ":" + formattedMinute + " PM";
-        } else {
-            int temp = hour - 12;
-            time = temp + ":" + formattedMinute + " PM";
-        }
-
-
-        return time;
-    }
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void setAlarm(String text, String date, String time) {
-        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        Intent intent = new Intent(getApplicationContext(), AlarmBroadcast.class);
-        intent.putExtra("event", text);
-        intent.putExtra("time", date);
-        intent.putExtra("date", time);
-
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
-        String dateandtime = date + " " + timeTonotify;
-        DateFormat formatter = new SimpleDateFormat("d-M-yyyy hh:mm");
-        try {
-            Date date1 = formatter.parse(dateandtime);
-            am.set(AlarmManager.RTC_WAKEUP, date1.getTime(), pendingIntent);
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        finish();
-
-    }
-}
+    }}
