5b73d31a07616ae8b6d7487579caa437a14b8bd9
Neil Mehta <neil.mehta310501@gmail.com>
2020-11-28 04:04:41 +0530

    UI changes made from square to rounded in notes and categories Notification Jugaad => made the title and content hard coded

--- app/src/main/java/com/bitspilani/inventorytrackerjava/Note/AddNote.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/Note/AddNote.java
@@ -1,9 +1,11 @@
 package com.bitspilani.inventorytrackerjava.Note;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,16 +23,24 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import java.text.ParseException;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +52,9 @@ public class AddNote extends AppCompatActivity {
     Intent data;
     Button button_date, button_time;
     String timeTonotify;
+    String dateString = "";
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+    SimpleDateFormat sdf2 = new SimpleDateFormat("hh:mm:ss dd-MM-yyyy");
 
 
 
@@ -57,13 +70,14 @@ public class AddNote extends AppCompatActivity {
         button_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AddNote.this, "Reminder", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(AddNote.this, ReminderBroadcast.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(AddNote.this,0,intent,0);
-                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                long timeAtButtonClick = System.currentTimeMillis();
-                long tenSecondsInMillis = 1000*10;
-                alarmManager.set(AlarmManager.RTC_WAKEUP,timeAtButtonClick+tenSecondsInMillis,pendingIntent);
+                setDate();
+            }
+        });
+
+        button_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTime();
             }
         });
 
@@ -82,6 +96,7 @@ public class AddNote extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String nTitle = noteTitle.getText().toString();
                 String ncontent = noteContent.getText().toString();
 
@@ -89,34 +104,65 @@ public class AddNote extends AppCompatActivity {
                     Toast.makeText(AddNote.this, "Cannot save notes with Empty Fields", Toast.LENGTH_SHORT).show();
                     return;
 
+                }else{
+                    progressBarSave.setVisibility(view.VISIBLE);
+
+                    //save note
+                    DocumentReference docref = fstore.collection("notes").document(user.getUid())
+                            .collection("myNotes").document();
+                    Map<String, Object> note = new HashMap<>();
+                    note.put("title", nTitle);
+                    note.put("content", ncontent);
+
+                    docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(AddNote.this, "Note Added", Toast.LENGTH_SHORT).show();
+                            onBackPressed();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AddNote.this, "Try again", Toast.LENGTH_SHORT).show();
+                            progressBarSave.setVisibility(view.VISIBLE);
+                        }
+                    });
+                   giveNotification(view,nTitle,ncontent);
+
                 }
 
-                progressBarSave.setVisibility(view.VISIBLE);
 
-                //save note
-                DocumentReference docref = fstore.collection("notes").document(user.getUid())
-                        .collection("myNotes").document();
-                Map<String, Object> note = new HashMap<>();
-                note.put("title", nTitle);
-                note.put("content", ncontent);
-
-                docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(AddNote.this, "Note Added", Toast.LENGTH_SHORT).show();
-                        onBackPressed();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AddNote.this, "Try again", Toast.LENGTH_SHORT).show();
-                        progressBarSave.setVisibility(view.VISIBLE);
-                    }
-                });
             }
         });
 //        To add a back button in the ad note activity
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void giveNotification(View v,String title,String content) {
+        try {
+            Toast.makeText(AddNote.this, "Reminder Set", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(AddNote.this, ReminderBroadcast.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(AddNote.this,0,intent,0);
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            long timeAtButtonClick = System.currentTimeMillis();
+            try {
+                Date date = sdf.parse(dateString);  //This line of code can blow up.
+                long delay = date.getTime()-timeAtButtonClick;
+                alarmManager.set(AlarmManager.RTC_WAKEUP,timeAtButtonClick+delay,pendingIntent);
+            }catch (Exception e){
+                if(e!=null){
+                    Date date = sdf2.parse(dateString);
+                    Log.e("dateString parse error",e.getMessage());
+                    long delay = date.getTime()-timeAtButtonClick;
+                    alarmManager.set(AlarmManager.RTC_WAKEUP,timeAtButtonClick+delay,pendingIntent);
+                }
+            }
+
+        }catch (Exception e){
+            Log.e("DateTime Error",e.getMessage());
+
+        }
+
     }
 
     @Override
@@ -135,6 +181,16 @@ public class AddNote extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+
+
+//TODO: Change the "LemuBit" occurances in the source code!
+
+
+
+
+
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "LemubitReminderChannel";
@@ -146,4 +202,61 @@ public class AddNote extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
 
-    }}
+    }
+
+    private void setTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                timeTonotify = i + ":" + i1;
+                button_time.setText(FormatTime(i, i1));
+            }
+        }, hour, minute, true);
+        timePickerDialog.show();
+
+    }
+
+    private void setDate() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = day + "-" + (month + 1) + "-" + year;
+                button_date.setText(date);
+                dateString += " " + date ;
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    public String FormatTime(int hour, int minute) {
+
+        String time;
+        time = "";
+        String formattedMinute;
+
+        if (minute / 10 == 0) {
+            formattedMinute = "0" + minute;
+        } else {
+            formattedMinute = "" + minute;
+        }
+
+
+        if (hour < 12) {
+            time = "0" + hour + ":" + formattedMinute + ":"  + "00";
+        }else{
+            time = hour +":" + formattedMinute + ":"+ "00";
+        }
+
+        dateString += " " + time;
+        return time;
+    }
+}
--- app/src/main/java/com/bitspilani/inventorytrackerjava/ReminderBroadcast.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/ReminderBroadcast.java
@@ -3,15 +3,36 @@ package com.bitspilani.inventorytrackerjava;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-public class ReminderBroadcast  extends BroadcastReceiver {
+public class ReminderBroadcast  extends BroadcastReceiver  {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,"Lemubit").setSmallIcon(R.drawable.ic_menu_camera).setContentTitle("Title").setContentText("Hello").setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-        notificationManagerCompat.notify(200,mBuilder.build());
+
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,"Lemubit")
+                    .setSmallIcon(R.drawable.ic_menu_camera)
+                    .setContentTitle("You have a TODO Scheduled")
+                    .setContentText("Checkout your Scheduled TODO Task right now!!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+            notificationManagerCompat.notify(200,mBuilder.build());
+
+
+
+
+        //TODO:Change the app Icon and also change the notification Icon.
+//        TOdo: Also change the Lemubit occurances in the code.
+
+
+
     }
 }
