c10885fb8c5d7dbad9ab3f9ae342eb60b5f14e20
Neil Mehta <neil.mehta310501@gmail.com>
2020-11-28 20:40:29 +0530

    dateTime String bug fixed. On Pressing notification it closes

--- app/src/main/java/com/bitspilani/inventorytrackerjava/ReminderBroadcast.java
+++ app/src/main/java/com/bitspilani/inventorytrackerjava/ReminderBroadcast.java
@@ -1,5 +1,6 @@
 package com.bitspilani.inventorytrackerjava;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -19,17 +20,20 @@ public class ReminderBroadcast  extends BroadcastReceiver  {
 
 
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,"Lemubit")
-                    .setSmallIcon(R.drawable.ic_menu_camera)
+                    .setSmallIcon(R.drawable.images2)
                     .setContentTitle("You have a TODO Scheduled")
                     .setContentText("Checkout your Scheduled TODO Task right now!!")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        PendingIntent conPendingIntent = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(conPendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
             notificationManagerCompat.notify(200,mBuilder.build());
 
 
 
 
-        //TODO:Change the app Icon and also change the notification Icon.
 //        TOdo: Also change the Lemubit occurances in the code.
 
 
